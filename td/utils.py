a427725d5d7dbe9387a532ae04ceb8f4dccf2b45
Théo HUET <theo6huet@gmail.com>
2026-02-12 20:06:31 +0100

    fix : utilisation de sparse et correction des resultats td1 ex2

--- td/utils.py
+++ td/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy import sparse
+
 import constants 
 
 # Random stuf
@@ -28,18 +30,23 @@ def norm2(v : np.array) :
 # phy
 
 def ground_state(H : np.array) : # calcul de l'énérgie et du vecteur propre associé au ground state
-  eigenValues, eigenVectors = np.linalg.eigh(H)
-  i = np.argwhere(eigenValues == np.min(eigenValues))[0][0]
+  eigenValues, eigenVector = sparse.linalg.eigsh(H, k=1, which="SA")  # k=1 cause we need one vector, "SA" for smallest eigenvalue
+  return eigenValues, eigenVector/np.linalg.norm(eigenVector)
+  """eigenValues, eigenVectors = np.linalg.eigh(H)
+  i = np.argmin(eigenValues)
   eigenVector = eigenVectors[:,i]
-  return eigenValues[i], eigenVector/np.linalg.norm(eigenVector)
+  return eigenValues[i], eigenVector/np.linalg.norm(eigenVector)"""
 
 def compute_rho(v : np.array) :
   return np.outer(v,np.conjugate(v))
   
 # SVD
 
-def svd(A : np.array) :
-  return np.linalg.svd(A, full_matrices=False) # return U, s, Vh
+def svd(A : np.array) : # return U, s, Vh
+  return np.linalg.svd(A, full_matrices=False)
+
+def sparse_svd(A : np.array, k: int) :
+  return sparse.linalg.svds(A, k)
 
 def mps_tensor(A : np.array, B :np.array):
   return np.einsum('sk,kS->sS', A, B) #return psi
@@ -53,13 +60,14 @@ def measurements_averageEnergy(H : np.array, rho : np.array) : # <H>
   return np.trace(H @ rho)
 
 def measurements_entropy(rho : np.array) : # S
-  eigenValues = np.abs(np.linalg.eigvals(rho)) + constants.epsilon  # with epsilon = 1e-8, this avoids numerical instability issues
+  eigenValues = np.abs(np.linalg.eigvals(rho)) # this avoids numerical instability issues : log(0)
+  eigenValues = eigenValues[eigenValues > 1e-12]
   return  -np.sum(eigenValues * np.log(eigenValues))
 
 def measurements_entropy_SVD(s : np.array) : # S en utilisant les valeurs singulière
-  s2 = np.abs(s)**2 + constants.epsilon  # with epsilon = 1e-8, this avoids numerical instability issues
+  s2 = np.abs(s)**2
+  s2 = s2[s2 > 1e-12] # this avoids numerical instability issues : log(0)
   return  -np.sum(s2 * np.log(s2))
 
 def measurements_purity(rho : np.array) : # P
   return np.trace(rho @ rho)
-
