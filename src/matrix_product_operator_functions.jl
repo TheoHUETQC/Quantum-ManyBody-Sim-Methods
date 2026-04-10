fafc85ff0e3be53e6e5564473e3b13fc51bdf58a
Théo HUET <theo6huet@gmail.com>
2026-04-10 21:57:24 +0200

    feat: working MPO home made package
    with overlap computation function, operator entropy computation function, propagate_layerbylayer function
    
    We can now propagate a ITensors.MPO in a circuit

--- src/matrix_product_operator_functions.jl
+++ src/matrix_product_operator_functions.jl
@@ -1,4 +1,90 @@
 module mpo_functions
-export propagate_layerbylayer
+export overlap, operator_entropy, propagate_layerbylayer
+
+using ITensors, ITensorMPS
+
+#------------ Overlap with a state psi ------------
+function overlap(O::MPO, psi::MPS)::Float64
+  return real(inner(psi, O, psi))
+end
+
+#------------ Operator Entropy ------------
+function compute_entropy(s)::Float64
+  p = s.^2 / sum(s.^2)
+  p = p[p .> 1e-18]
+  return -sum(p .* log.(p))
+end
+
+function operator_entropy(O::MPO, bond::Int)::Float64
+    if bond == 1 # on ne peut pas couper au bond 1 (pas de lien à gauche)
+        return 0.0
+    end
+    
+    O_ortho = orthogonalize(O, bond)
+  
+    l_link = linkind(O_ortho, bond-1)
+    s_inds = siteinds(O_ortho, bond)
+    
+    # SVD, on sépare le lien gauche et les indices physiques
+    U, S, V = svd(O_ortho[bond], l_link, s_inds...)
+    
+    s_vals = diag(S)
+    return compute_entropy(s_vals)
+end
+
+#------------ Propagate Layer by layer ------------ 
+function propagate_layerbylayer(
+    circuit::Vector{Vector{ITensor}},
+    observable::MPO;
+    cutoff::Float64=1e-8,
+    bond::Union{Int, Nothing}=nothing
+    )
+  t0 = time()
+  nlayers = length(circuit)
+
+  maxbonds, entropies, norms, overlaps = Int[], Float64[], Float64[], Float64[]
+  current = observable
+  adjoint_circuit = [dag.(layer) for layer in circuit]
+
+  sites_mps = siteinds(first, current)
+  N = length(sites_mps)
+  if bond === nothing 
+    bond = N ÷ 2 # L'entropie est mesurée au milieu de la chaine par defaut
+  end
+
+  init_state = ["Up" for _ in 1:N] # "Dn" pour down
+  psi0 = MPS(sites_mps, init_state)
+
+  norm0 = norm(observable)
+
+  for (layer_idx, layer) in enumerate(adjoint_circuit)
+    current = apply(layer, current; apply_dag=true, cutoff=cutoff) # apply(U,0,apply_dag=true) fait U O U+ donc on applique d'abord le dag a layer pour avoir +U O U
+    
+    maxbond_temp = maxlinkdim(current)
+
+    push!(maxbonds, maxbond_temp)
+    push!(entropies, operator_entropy(current, bond))
+    push!(overlaps, overlap(current, psi0))
+
+    if layer_idx % max(1, nlayers ÷ 10)==0
+        norm_temp = norm(current)
+        push!(norms, norm_temp)
+
+        if !isapprox(norm_temp, norm0; rtol=1e-3) # on verifie que la norme ne diverge pas trop
+          println("layer : $layer_idx /$nlayers Break cause norm = $norm_temp ≠ $norm0")
+          break
+        end
+
+        println("layer : $layer_idx /$nlayers complete")
+    end
+  end
+
+  elapsed_time = time() - t0
+  println("Time taken by mpo_functions.propagate_layerbylayer: ", elapsed_time, " seconds")
+
+  result = Dict("maxbond" => maxbonds, "S" => entropies, "norm" => norms, "overlap" => overlaps)
+
+  return current, result
+end
 
 end # module
