b28a62a57ba6990f04f8e6f82682afc29543b441
Théo HUET <theo6huet@gmail.com>
2026-04-13 19:53:20 +0200

    fix: initial state condition psi0

--- src/matrix_product_operator_functions.jl
+++ src/matrix_product_operator_functions.jl
@@ -4,8 +4,8 @@ export overlap, operator_entropy, propagate_layerbylayer
 using ITensors, ITensorMPS
 
 #------------ Overlap with a state psi ------------
-function overlap(O::MPO, psi::MPS)::Float64
-  return real(inner(psi, O, psi))
+function overlap(O::MPO, ψ::MPS)::Float64
+  return real(inner(ψ, O, ψ))
 end
 
 #------------ Operator Entropy ------------
@@ -37,26 +37,31 @@ function propagate_layerbylayer(
     circuit::Vector{Vector{ITensor}},
     observable::MPO;
     cutoff::Float64=1e-8,
-    bond::Union{Int, Nothing}=nothing
+    bond::Union{Int, Nothing}=nothing,
+    ψ0::Union{MPS, Nothing}=nothing
     )
   t0 = time()
   nlayers = length(circuit)
-
+  
   maxbonds, entropies, norms, overlaps = Int[], Float64[], Float64[], Float64[]
-  current = observable
-  adjoint_circuit = [dag.(layer) for layer in circuit]
 
+  current = copy(observable)
+  adjoint_circuit = [dag.(layer) for layer in circuit]
+  
   sites_mps = siteinds(first, current)
+
   N = length(sites_mps)
   if bond === nothing 
     bond = N ÷ 2 # L'entropie est mesurée au milieu de la chaine par defaut
   end
 
-  init_state = ["Up" for _ in 1:N] # "Dn" pour down
-  psi0 = MPS(sites_mps, init_state)
+  if ψ0 === nothing
+    init_state = ["Up" for _ in 1:N] # "Dn" pour down
+    ψ0 = MPS(sites_mps, init_state)
+  end
 
-  norm0 = norm(observable)
-
+  norm0 = norm(observable)  
+  push!(overlaps, overlap(observable, ψ0))
   for (layer_idx, layer) in enumerate(adjoint_circuit)
     current = apply(layer, current; apply_dag=true, cutoff=cutoff) # apply(U,0,apply_dag=true) fait U O U+ donc on applique d'abord le dag a layer pour avoir +U O U
     
@@ -64,9 +69,9 @@ function propagate_layerbylayer(
 
     push!(maxbonds, maxbond_temp)
     push!(entropies, operator_entropy(current, bond))
-    push!(overlaps, overlap(current, psi0))
+    push!(overlaps, overlap(current, ψ0))
 
-    if layer_idx % max(1, nlayers ÷ 10)==0
+    if layer_idx % (nlayers ÷ 10)==0
         norm_temp = norm(current)
         push!(norms, norm_temp)
 
