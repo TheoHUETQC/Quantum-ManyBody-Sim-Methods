35de422a919437fc7a23e433d648e59d0dbdfb39
Théo HUET <theo6huet@gmail.com>
2026-06-09 22:34:56 +0200

    fix : delete the opetor entropy func and add the apply_depolarizing_noise

--- src/exact_functions.jl
+++ src/exact_functions.jl
@@ -1,5 +1,5 @@
 module exact_functions
-export overlap, operator_entropy, propagate_layerbylayer, get_Zi
+export overlap, apply_depolarizing_noise, propagate_layerbylayer, get_Zi
 
 using LinearAlgebra
 
@@ -52,48 +52,6 @@ function overlap(O::Matrix, ψ::Union{Vector{Float64}, Vector{Int64}})::Float64
     This function performs the matrix-vector multiplication $\langle \psi | \hat{O} | \psi \rangle$. It returns the real part of the result, which is appropriate for physical observables represented by Hermitian matrices.
     """
     return real(ψ' * O * ψ)
-end
-
-#------------ Operator Entropy ------------
-function operator_entropy(O::Matrix, bond::Int)::Float64
-    raw"""
-    operator_entropy(O::Matrix, bond::Int)::Float64
-
-    Calculate the entanglement (Shannon) entropy of a dense operator `O` at a specified partition bond.
-
-    ### Arguments
-
-    * `O`: The dense matrix representation of the operator.
-    * `bond`: The number of qubits in the left partition (the cut location).
-
-    ### Returns
-
-    * The entropy value as a `Float64`.
-
-    ### Notes
-
-    This function reshapes the $2^n \times 2^n$ matrix into a tensor partitioned into $L$ and $R$ subsystems. It performs a Singular Value Decomposition (SVD) on the reshaped matrix to extract the singular value spectrum. The entropy is then calculated from the normalized squared singular values, with a small regularization constant ($10^{-15}$) to ensure numerical stability during the logarithmic calculation.
-    """
-    dim_total = size(O, 1)
-    nqubits = Int(log2(dim_total))
-    
-    if bond < 1 || bond >= nqubits # on ne peut pas couper au bond 1 (pas de lien à gauche)
-        return 0.0
-    end
-
-    # dL = dimension à gauche, dR = dimension à droite
-    dL = 2^bond
-    dR = 2^(nqubits - bond)
-
-    O_tensor = reshape(O, dR, dL, dR, dL)
-    O_perm = permutedims(O_tensor, (2, 4, 1, 3))
-    M_schmidt = reshape(O_perm, dL^2, dR^2)
-    s = svdvals(M_schmidt)
-
-    prob = s.^2 / sum(s.^2)
-    entropy = -sum(p * log(p + 1e-15) for p in prob)
-
-    return entropy
 end
 
 #------------ Noise layer ------------
